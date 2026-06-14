40f242c1f364f27696629f13bc2478a1a1d16616
Pulin <pulin.sahoo2010@gmail.com>
2026-06-14 21:06:39 +0530

    updateed StartCoding

--- StartCoding.java
+++ StartCoding.java
@@ -2,5 +2,6 @@ public class StartCoding {
     public static void main(String args[])
     {
         System.out.println("This is the beginning ");
+        System.out.println("Let see if it is pushing now ");
     }
 }
